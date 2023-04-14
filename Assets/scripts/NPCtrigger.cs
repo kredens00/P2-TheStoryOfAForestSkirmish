20b31215a66124f64068fb786be4206161a2dadb
kredens00 <80453773+kredens00@users.noreply.github.com>
2023-04-14 03:36:52 +0200

    Pause/Exit; NPC dialog

---
+++ Assets/scripts/NPCtrigger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCtrigger : MonoBehaviour
+{
+    private GameObject triggeringNpc;
+    private bool triggering;
+
+    public GameObject npcText;
+    public GameObject npcText2;
+
+
+    void Update()
+    {
+        if(triggering)
+        { 
+
+            if (Input.GetKeyUp(KeyCode.E))
+           {
+                npcText.SetActive(false);
+                npcText2.SetActive(true);
+           }
+        
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.tag == "NPC")
+        {
+            triggering = true;
+            triggeringNpc = other.gameObject;
+            npcText.SetActive(true);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            triggering = false;
+            triggeringNpc = null;
+            npcText.SetActive(false);
+            npcText2.SetActive(false);
+        }
+    }
+}
+
--- Assets/scripts/follow.cs
+++ Assets/scripts/follow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class follow : MonoBehaviour
 {
 
     public GameObject player;
