f52542321ea49f3bcee0be40c0821237e6130db9
Nusiek <norbert.furmanik@op.pl>
2023-10-23 17:57:58 +0200

    Tła i deszcz
    Poprawione tła i deszcz się ładuje raz na początku i particle nie niszczy się po przejściu na inną scenę

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+
+    public ParticleSystem particleSystem;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (particleSystem != null)
+        {
+            particleSystem.Play();
+        }
+    }
+}
