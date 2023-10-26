197b8954d9fab9a94931797923fc861675eedb73
Nusiek <norbert.furmanik@op.pl>
2023-10-26 21:20:56 +0200

    Dodane particle dymu i iskier do ognisk i poprawiony particle deszczu

--- Assets/DontDestroyRainOnLoad.cs
+++ Assets/DontDestroyRainOnLoad.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyOnLoad : MonoBehaviour
+public class DontDestroyRainOnLoad : MonoBehaviour
 {
 
     public ParticleSystem particleSystem;
