72ab0f03a66c131dd6d0cf53cb3e27cbb9d85b6b
Nusiek <norbert.furmanik@op.pl>
2023-11-07 18:34:08 +0100

    dodane unity analitics

--- Assets/RainActivation.cs
+++ Assets/RainActivation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyRainOnLoad : MonoBehaviour
+public class RainActivation : MonoBehaviour
 {
 
     public ParticleSystem particleSystem;
