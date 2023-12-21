b9fd5910b487c5e29ff1e5eb63019b0449ab4a9b
Piotr Szczepanek <79981833+DobrzePaniePiotrze@users.noreply.github.com>
2023-12-21 20:25:51 +0100

    Zablokowanie scrolla w UI

---
+++ Assets/Scripts/DisableScroll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableScroll : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+
+    void Start()
+    {
+        // Pobierz komponent ScrollRect z obiektu
+        scrollRect = GetComponent<ScrollRect>();
+
+        if (scrollRect != null)
+        {
+            // Ustaw czułość scrollowania na 0, co praktycznie wyłączy scrollowanie
+            scrollRect.scrollSensitivity = 0f;
+        }
+        else
+        {
+            Debug.LogWarning("ScrollRect component not found on this GameObject!");
+        }
+    }
+}
